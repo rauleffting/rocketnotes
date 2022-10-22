cd3a894b0440308d58ce275b42be0153b815c05b
Raul Francisco Effting <rauleffting@Rauls-iMac.lan>
2022-10-22 18:40:13 -0300

    projeto criado

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,11 @@
+import { Container } from './styles.js';
+
+export function Details() {
+    
+    return(
+        <Container>
+            <h1>Hello World!</h1>
+            <span>Raul Effting</span>
+        </Container>
+    )
+}
