14c7b26d79a5f8c2bcb870fc3ec5adf0a66e5c47
Raul Francisco Effting <rauleffting@Rauls-iMac.lan>
2022-10-25 11:23:20 -0300

    add button

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,4 +1,5 @@
 import { Container } from './styles.js';
+import { Button } from '../../components/Button';
 
 export function Details() {
     
@@ -6,6 +7,7 @@ export function Details() {
         <Container>
             <h1>Hello World!</h1>
             <span>Raul Effting</span>
+            <Button title="Login" loading/>
         </Container>
     )
 }
