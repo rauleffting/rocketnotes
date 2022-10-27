e160aa54bc092f8eaa8ebcc508e7a15105054fed
Raul Francisco Effting <rauleffting@Rauls-iMac.lan>
2022-10-27 13:48:27 -0300

    front-end finished

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import { FiPlus, FiSearch } from 'react-icons/fi';
+import { FiPlus } from 'react-icons/fi';
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
 
 import { Header } from '../../components/Header';
@@ -39,7 +39,7 @@ export function Home() {
         </Section>
       </Content>
 
-      <NewNote>
+      <NewNote to="/new">
         <FiPlus />
         Criar nota
       </NewNote>
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const Container = styled.div`
   width: 100%;
@@ -58,11 +59,11 @@ export const Content = styled.div`
   overflow-y: auto; /* se tiver muitas notas, ele criar um scroll */
 `;
 
-export const NewNote = styled.button`
+export const NewNote = styled(Link)`
   grid-area: newnote;
   
   background: ${({ theme }) => theme.COLORS.ORANGE };
-  border: none;
+  color: ${({ theme }) => theme.COLORS.BACKGROUND_900 };
   
   display: flex;
   align-items: center;
