0f50deb82b7b81d2a06d091e0f56f6e3ccbeefc3
Raul Francisco Effting <rauleffting94@gmail.com>
2022-12-23 13:11:56 -0300

    changed api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3333" /* "https://rocketnotes-api94.herokuapp.com"  */
+  baseURL: "https://rocketnotes-api-er9i.onrender.com"  
 });
